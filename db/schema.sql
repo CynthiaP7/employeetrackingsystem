5adc6ad00d008b24520e452295a31517100f1aeb
Cynthia P <cynthiads417@gmail.com>
2023-10-22 17:06:28 -0400

    add seeds and data

--- db/schema.sql
+++ db/schema.sql
@@ -4,10 +4,30 @@ CREATE DATABASE work_db;
 USE work_db;
 
 CREATE TABLE department (
-    id INT NOT NULL  AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
 
-)
+CREATE TABLE employee(
+    id INT UNASSIGNED AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR (30) NOT NULL,
+    last_name VARCHAR (30) NOT NULL,
+    role_id INT UNSIGNED NOT NULL,
+    manager_id NULL,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
+    ON DELETE SET NULL
+
+
+);
---
+++ db/seeds.sql
@@ -0,0 +1,19 @@
+INSERT INTO department (name)
+VALUES ("Engineering"),
+       ("Sales"),
+       ("Legal"),
+       ("Finance");
+
+INSERT INTO role (tile, salary, department_id)
+VALUES (""),
+       (""),
+       (""),
+       (""),
+       (""),
+       (""),
+       (""),
+       (""),
+
+INSERT INTO employee ()
+VALUES
+        
