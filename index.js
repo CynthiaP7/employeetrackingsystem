80b35d8e5e677ece73ab50d7db3550205ae34b3e
Cynthia P <cynthiads417@gmail.com>
2023-10-22 22:54:29 -0400

    set up connections

--- index.js
+++ index.js
@@ -20,4 +20,61 @@ const db = mysql.createConnection(
     console.log(`Connected to the work_db database.`)
   );
 
-  
+  const userPrompt = new Prompt();
+  const userQuery = new Queries(db);
+
+  const init = async () => {
+    const ans = await userPrompt.prompt();
+    switch (ans.option) {
+        case "View All Deparments":
+          userQuery.viewAllDepartments();
+          break;
+    
+        case "View All Roles":
+          userQuery.viewAllRoles();
+          break;
+        
+        case "View All Employees":
+          userQuery.viewAllEmployees();
+          break;
+
+        case "Add Department":
+          const departmentAnswers = await userPrompt.addDepartmentPrompt();
+          userQuery.addDepartment(departmentAnswers);
+          userQuery.viewAllDepartments();
+          break;
+
+        case "Add Role":
+            const departmentChoices = await userQuery.getDepartmentChoices();
+            const roleAnswers = await userPrompt.addRolePrompt(departmentChoices);
+            userQuery.addRole(roleAnswers);
+            userQuery.viewAllRoles();
+            break;
+
+        case "Add Employee":
+            const roleChoices = await userQuery.getRoleChoices();
+            const managerChoices = await userQuery.getManagerChoices()    
+            const employeeAnswers = await userPrompt.addEmployeePrompt(roleChoices, managerChoices);
+            userQuery.addEmployee(employeeAnswers);
+            userQuery.viewAllEmployees();
+            break;
+         
+
+        case "Update Employee Role":
+            const employeeNames = await userQuery.getEmployeeNames();
+            const employeeRoleChoices = await userQuery.getRoleChoices()    
+            const updateEmployeeAnswers = await userPrompt.updateEmployeePrompt(employeeNames, employeeRoleChoices);
+            userQuery.updateEmployee(updateEmployeeAnswers);
+            userQuery.viewAllEmployees();
+          break;
+    
+    default:
+        process.exit(0);
+        return;
+    }
+    init();
+  };
+
+  init()
+
+  module.exports =db;
